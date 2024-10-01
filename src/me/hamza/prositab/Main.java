c1df561223dfb38d5b7c9b119b78128ddebcc5cb
HamzaBHmida <hamzabenhmida987@gmail.com>
2024-10-01 12:15:34 +0200

    new one
    second

--- src/me/hamza/prositab/Main.java
+++ src/me/hamza/prositab/Main.java
@@ -4,10 +4,33 @@ import me.hamza.prositab.utils.*;
 
 public class Main {
     public static void main(String[] args) {
-        Animal y = new Animal("Lion","Feline",12,false);
-        Zoo x = new Zoo("Manar","Tunis",20);
-        x.displayZoo();
-        y.displayAnimal();
 
+        boolean check,checke,t;
+        int search,s;
+
+        Animal lion = new Animal("Scar", "Feline", 50, false);
+        Animal dog = new Animal("Dog", "Mamifere", 10, false);
+        Animal lionn = new Animal("Scar", "Feline", 50, false);
+        Animal lionnn = new Animal("Scarface", "Feline", 20, false);
+        Animal tiger = new Animal("Milky", "Feline", 13, true);
+        Animal owl = new Animal("Quasimodo", "Bird", 8, true);
+        Animal elephant = new Animal("Terrence", "Mamifere", 9, true);
+        Zoo tunis = new Zoo("International Zoo of Ariana", "Tunis");
+        Zoo sfax = new Zoo("International Zoo of Sfax", "Sfax");
+
+
+
+        tunis.addAnimal(lion);
+        tunis.addAnimal(lionn);
+        tunis.addAnimal(lionnn);
+        tunis.addAnimal(tiger);
+        tunis.addAnimal(owl);
+        tunis.addAnimal(elephant);
+        tunis.removeAnimal(dog);
+        sfax.addAnimal(dog);
+        Zoo.compareZoo(tunis,sfax);
+        tunis.displayZoo();
+        sfax.displayZoo();
+        System.out.println(tunis.isZooFull());
     }
 }
